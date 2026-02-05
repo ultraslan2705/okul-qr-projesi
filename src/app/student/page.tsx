eb4184a9bda186ec885c3cdd99d68e60edf0854b
GOKUTA <gokuta27@192.168.1.5>
2026-02-05 21:45:06 +0300

    Make student page dynamic

--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { getTeachers } from "@/lib/teachers";
 import { getSettings } from "@/lib/settings";
 
+export const dynamic = "force-dynamic";
+
 export default async function StudentPage() {
   const [settings, teachers] = await Promise.all([getSettings(), getTeachers()]);
 
